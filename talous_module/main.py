1776f297c42a2e1648665e20ae2afeb31c9011d6
Jan Härkönen <jan.harkonen@gmail.com>
2024-06-28 00:29:35 +0300

    Poistettu turha kommentti

--- talous_module/main.py
+++ talous_module/main.py
@@ -2,7 +2,6 @@ from helper_functions import *
 from cryptocom_verolaskelmat import *
 from DataParseClasses import *
 
-#print(__name__)
 #assert sum([1, 2, 3]) == 6, "Should be 6"
 INPUT_FILE = 'D:/Python-projektit/yksityinen/crypto_transactions_record_20240519_155200.csv'
 OUTPUT_FILE = 'output_2024.csv'
