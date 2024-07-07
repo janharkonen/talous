9b20b56d2c41d36368e7a5bff8a654d7f2470b0f
Jan Härkönen <jan.harkonen@gmail.com>
2024-07-07 23:02:32 +0300

    @0014-Toteuta_RefinedDataWriter

--- talous_module/main.py
+++ talous_module/main.py
@@ -3,14 +3,15 @@ from cryptocom_verolaskelmat import *
 from DataParseClasses import *
 
 #assert sum([1, 2, 3]) == 6, "Should be 6"
-INPUT_FILE = 'D:/Python-projektit/yksityinen/crypto_transactions_record_test.csv'
+INPUT_FILE = 'D:/Python-projektit/yksityinen/crypto_transactions_record_test_real_nums.csv'
 OUTPUT_FILE = 'output_2024.csv'
 pd.set_option('display.max_rows', None)
 def main():
 
-    RawData1 = CsvToRawData(INPUT_FILE)
-    ARefinedData = RefinedData(RawData1)
-    ARefinedData.df
+    raw_data = CsvToRawData(INPUT_FILE)
+    refined_data = RefinedData(raw_data)
+    writer = RefinedDataWriter(refined_data, OUTPUT_FILE)
+    writer.run()
 
 if __name__ == '__main__':
     main()
